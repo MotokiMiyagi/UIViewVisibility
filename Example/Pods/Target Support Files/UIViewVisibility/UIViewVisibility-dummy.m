74cd9d8a734be23532d860f722bc396be85a6280
m-miyagi <m-miyagi@omwonlab.com>
2016-09-15 10:53:21 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/UIViewVisibility/UIViewVisibility-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIViewVisibility : NSObject
+@end
+@implementation PodsDummy_UIViewVisibility
+@end
---
+++ Example/Pods/Target Support Files/UIViewVisibility/UIViewVisibility-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double UIViewVisibilityVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIViewVisibilityVersionString[];
+
